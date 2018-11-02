e21940cf4b249341d661376b4c6cce19448a9a33
hantarr01 <thunderbolt655@mail.ru>
2018-11-02 22:36:06 +0300

    006

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+$(document).ready(function(){
+$('.slider').bxSlider({
+auto: true,
+autoControls: true,
+stopAutoOnClick: true,
+pager: true,
+slideWidth: 300
+});
+});
+
+
 function poisk() {
   alert('Поиск на данный момент недоступен!');
 }
+
+
