8c6c3d4990e9a35f4b216e0c66df9d2ee5be345c
hantarr01 <thunderbolt655@mail.ru>
2018-11-05 12:50:43 +0300

    007

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 
-$(document).ready(function(){
-$('.slider').bxSlider({
-auto: true,
-autoControls: true,
-stopAutoOnClick: true,
-pager: true,
-slideWidth: 300
-});
-});
-
-
 function poisk() {
   alert('Поиск на данный момент недоступен!');
 }
 
+$(function() {
+    $(".rslides").responsiveSlides();
+  });
 
